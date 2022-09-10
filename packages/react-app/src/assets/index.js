cc744fbd434ab3c12b691011283f99a416b3c7c1
Krishna varma <kvarma00011@gmail.com>
2022-09-11 02:28:22 +0530

    Day 2 - Done with home layout

--- packages/react-app/src/assets/index.js
+++ packages/react-app/src/assets/index.js
@@ -1,5 +1,6 @@
 import chevronDown from "./chevron-down.svg";
 import ethereumLogo from "./ethereumLogo.png";
 import uniswapLogo from "./uniswapLogo.png";
+import paypal1 from "./paypal1.jpg";
 
-export { chevronDown, ethereumLogo, uniswapLogo };
+export { chevronDown, ethereumLogo, uniswapLogo, paypal1 };
